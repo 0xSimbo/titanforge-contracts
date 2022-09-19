29a15b1fb85235932a8a492dde92e6d8019f0715
simon <simonnfts@gmail.com>
2022-09-19 00:11:08 -0400

    chore: commented out titans facet. need to rework later

--- contracts/TitanforgeDiamond/libraries/LibTitanMint.sol
+++ contracts/TitanforgeDiamond/libraries/LibTitanMint.sol
@@ -1,48 +1,48 @@
-// SPDX-License-Identifier: Unlicensed
-pragma solidity 0.8.17;
-import "./LibAppStorage.sol";
-import "../interfaces/IAppStorage.sol";
-import "../../interfaces/IKobolds.sol";
-import "../../interfaces/IIngotToken.sol";
-import "./LibKoboldMultipliers.sol";
-import "../interfaces/IKoboldMultiplier.sol";
-import "../interfaces/Class.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-library LibTitanMint {
-    using ECDSA for bytes32;
-    //Storage
-    bytes32 internal constant NAMESPACE = keccak256("titanforge.titan.mint");
-    uint256 internal constant MAX_SUPPLY = 8888;
+// // SPDX-License-Identifier: Unlicensed
+// pragma solidity 0.8.17;
+// import "./LibAppStorage.sol";
+// import "../interfaces/IAppStorage.sol";
+// import "../../interfaces/IKobolds.sol";
+// import "../../interfaces/IIngotToken.sol";
+// import "./LibKoboldMultipliers.sol";
+// import "../interfaces/IKoboldMultiplier.sol";
+// import "../interfaces/Class.sol";
+// import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+// library LibTitanMint {
+//     using ECDSA for bytes32;
+//     //Storage
+//     bytes32 internal constant NAMESPACE = keccak256("titanforge.titan.mint");
+//     uint256 internal constant MAX_SUPPLY = 8888;
 
-    struct Storage{
-        uint256 acceptableTimelag;
-        uint256 shardsRequiredToMintOne;
-        uint256  ingotRequiredToMintOne;
-        uint256 numKoboldsRequiredToMintOne;
-        address signer;
-        bool revealed;
-        string baseURI;
-        string uriSuffix;
-        string notRevealedUri;
-        mapping(uint256=>Class) titanToClass;
-        mapping(uint256 => Class) class;
+//     struct Storage{
+//         uint256 acceptableTimelag;
+//         uint256 shardsRequiredToMintOne;
+//         uint256  ingotRequiredToMintOne;
+//         uint256 numKoboldsRequiredToMintOne;
+//         address signer;
+//         bool revealed;
+//         string baseURI;
+//         string uriSuffix;
+//         string notRevealedUri;
+//         mapping(uint256=>Class) titanToClass;
+//         mapping(uint256 => Class) class;
 
         
-    }
+//     }
     
-    function getStorage() internal pure returns(Storage storage s)  {
-        bytes32 position = NAMESPACE;
-        assembly{
-            s.slot := position
-        }
-    }
+//     function getStorage() internal pure returns(Storage storage s)  {
+//         bytes32 position = NAMESPACE;
+//         assembly{
+//             s.slot := position
+//         }
+//     }
    
 
 
     
     
     
-}
+// }
 
 
 
